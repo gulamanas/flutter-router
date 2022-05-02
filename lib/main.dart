f12c01c2777ccf23a3ad89d2d1253f583d12ff2c
Gulam Anas <g.anas786@gmail.com>
2022-05-02 21:38:47 +0530

    getting back to first screen

--- lib/main.dart
+++ lib/main.dart
@@ -15,41 +15,43 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   var _nameFieldController = TextEditingController();
+
+  Future _goToNextScreen(BuildContext context) async {
+    Map results = await Navigator.of(context)
+        .push(MaterialPageRoute<dynamic>(builder: (BuildContext context) {
+      return NextScreen(name: _nameFieldController.text);
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("First Screen"),
         centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: () {},
-             icon: Icon(Icons.settings)
-          )
-        ],
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.settings))],
       ),
       body: ListView(
         children: [
           ListTile(
             title: TextField(
               controller: _nameFieldController,
-              decoration: InputDecoration(
-                labelText: "Enter Your Name" 
-              ),
+              decoration: InputDecoration(labelText: "Enter Your Name"),
             ),
           ),
           ListTile(
             title: ElevatedButton(
               onPressed: () {
-                var router = MaterialPageRoute(
-                  builder: (BuildContext context) {
-                    return NextScreen(name: _nameFieldController.text,);
-                  });
+                var router = MaterialPageRoute(builder: (BuildContext context) {
+                  return NextScreen(
+                    name: _nameFieldController.text,
+                  );
+                });
 
-                  Navigator.of(context).push(router);
+                Navigator.of(context).push(router);
               },
-              child: Text("Send to Second Screen"), 
-              ),
+              child: Text("Send to Second Screen"),
+            ),
           )
         ],
       ),
@@ -59,7 +61,7 @@ class _HomeState extends State<Home> {
 
 class NextScreen extends StatefulWidget {
   final String name;
-  const NextScreen({ Key? key, required this.name }) : super(key: key);
+  const NextScreen({Key? key, required this.name}) : super(key: key);
 
   @override
   State<NextScreen> createState() => _NextScreenState();
@@ -67,6 +69,7 @@ class NextScreen extends StatefulWidget {
 
 class _NextScreenState extends State<NextScreen> {
   var _backTextFieldController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -81,14 +84,14 @@ class _NextScreenState extends State<NextScreen> {
           ),
           TextField(
             controller: _backTextFieldController,
-
           ),
           ElevatedButton(
-            onPressed: (() {
-              Navigator.pop(context, );
-            }),
-             child: Text('Send Back')
-            )
+              onPressed: (() {
+                Navigator.pop(context, {
+                  'info': _backTextFieldController.text,
+                });
+              }),
+              child: Text('Send Back'))
         ],
       ),
     );
